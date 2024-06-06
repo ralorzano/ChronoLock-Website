0eaa9fc63060b983808afc5ac2428f6058fdbdcb
Ralph Lorzano <ralorzano@my.cspc.edu.ph>
2024-06-06 22:26:14 +0800

    Additional sa last commit - 2

--- theme/admin-pendingrfid.php
+++ theme/admin-pendingrfid.php
@@ -16,42 +16,7 @@
 
   <title>ChronoLock Admin-Pending RFID</title>
 
-  <!-- BOOTSTRAP 5.3.3 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-
-  <!-- GOOGLE FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet" />
-  <link href="plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
-  <link href="plugins/simplebar/simplebar.css" rel="stylesheet" />
-
-  <!-- PLUGINS CSS STYLE -->
-  <link href="plugins/nprogress/nprogress.css" rel="stylesheet" />
-
-  <link href="plugins/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
-
-  <link href="plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
-
-  <!-- MONO CSS -->
-  <link id="main-css-href" rel="stylesheet" href="css/style.css" />
-
-  <!-- FAVICON -->
-  <link href="images/favicon.png" rel="shortcut icon" />
-
-  <!-- CALENDAR -->
-  <link href="plugins/fullcalendar/core-4.3.1/main.min.css" rel="stylesheet">
-  <link href="plugins/fullcalendar/daygrid-4.3.0/main.min.css" rel="stylesheet">
-
-  <!--
-    HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
-  -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  <script src="plugins/nprogress/nprogress.js"></script>
+  <?php include 'head.php' ?>
 </head>
 
 <body class="navbar-fixed sidebar-fixed" id="body">
--- theme/index.php
+++ theme/index.php
@@ -6,7 +6,7 @@
  // FACEBOOK: https://www.facebook.com/themefisher
  // GITHUB: https://github.com/themefisher/
 -->
-<!-- hello -->
+
 <html lang="en" dir="ltr">
 
 <head>
@@ -16,32 +16,8 @@
 
   <title>ChronoLock Admin Dashboard</title>
 
-  <!-- BOOTSTRAP 5.3.3 -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-  <!-- GOOGLE FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Karla:400,700|Roboto" rel="stylesheet" />
-  <link href="plugins/material/css/materialdesignicons.min.css" rel="stylesheet" />
-  <link href="plugins/simplebar/simplebar.css" rel="stylesheet" />
-
-  <!-- PLUGINS CSS STYLE -->
-  <link href="plugins/nprogress/nprogress.css" rel="stylesheet" />
-
-  <link href="plugins/jvectormap/jquery-jvectormap-2.0.3.css" rel="stylesheet" />
-
-  <link href="plugins/daterangepicker/daterangepicker.css" rel="stylesheet" />
-
-  <!-- MONO CSS -->
-  <link id="main-css-href" rel="stylesheet" href="css/style.css" />
-
-  <!-- FAVICON -->
-  <link href="images/favicon.png" rel="shortcut icon" />
-
-  <!-- CALENDAR -->
-  <link href="plugins/fullcalendar/core-4.3.1/main.min.css" rel="stylesheet">
-  <link href="plugins/fullcalendar/daygrid-4.3.0/main.min.css" rel="stylesheet">
-
+  <?php include 'head.php' ?>
+  
   <!-- TOASTER -->
   <link href="plugins/toaster/toastr.min.css" rel="stylesheet">
 
@@ -51,8 +27,6 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
   <!-- Custom JS -->
   <script src="js/toastr.js"></script>
-
-  <script src="plugins/nprogress/nprogress.js"></script>
 </head>
 
 <body class="navbar-fixed sidebar-fixed" id="body">
